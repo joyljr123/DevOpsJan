2d6ff7a64d75d50561cd51b7cfd765e3cd51c0bd
john liu <johnliu0523@gmail.com>
2020-01-27 11:25:19 -0800

    added tests classes

--- mavenPractice/src/test/java/calculatorTest.java
+++ mavenPractice/src/test/java/calculatorTest.java
@@ -1,6 +1,7 @@
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class calculatorTest {
 
@@ -12,8 +13,27 @@ public class calculatorTest {
         Integer expResult = 30;
         Integer result = Calculator.sum(x, y);
         assertEquals(expResult, result);
+
+        x = 10;
+        y = 20;
+        result = Calculator.sum(x, y);
+        assertEquals(expResult, result);
+
+    }
+    @Test
+    public void testSumNull() {
         assertEquals(null, Calculator.sum(15, null));
         assertEquals(null, Calculator.sum(null, 15));
         assertEquals(null, Calculator.sum(null, null));
+        assertNull(Calculator.sum(null, null));
     }
+
+    @Test
+    public void addLargetNum() {
+        Integer result = Calculator.sum(Integer.MAX_VALUE , Integer.MIN_VALUE);
+        assertNotNull(result);
+        Integer exp = -1;
+        assertEquals(exp, result);
+    }
+
 }
